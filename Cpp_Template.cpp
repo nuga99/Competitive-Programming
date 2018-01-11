ed9edabf9d8b319382bd1a12db8871f78d38777a
Muhammad Ardivan Satrio Nugroho <nugamuhammad@gmail.com>
2018-01-11 12:46:05 +0700

    Create Cpp_Template.cpp

---
+++ Cpp_Template.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+/** MACROS **/
+#define REP(i,a,b) for (int i = int(a); i <= int(b); i++)  // a ke b, dengan i variabel local
+#define TRvi(c,it) for (vi::iterator it = (c).begin(); it!=(c).end();it++);
+
+#define pb push_back
+
+#define INF 2000000000 // 2 milliar
+#define MEMSET_INF 127 // sekitar 2 miliar
+#define MEMSET_HALF_INF 63 //sekitar 1 miliar
+#define MOD 10000009 // 1e9
+
+typedef long long ll;
+
+using namespace std;
+
+struct LClass{
+	
+	/* Struct like class but more convenient */
+	//int, string, char, etc
+};
+
+int main(){
+
+
+return 0;
+}
